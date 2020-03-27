c2f25bb45c16936c96eaeab176077ecce1afc163
bgauduch <bgauduch@users.noreply.github.com>
2020-03-27 18:31:12 +0100

    :pencil: updated readme, corrected dev-up script and docker-compose

--- dev-up.sh
+++ dev-up.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 # build local image
 docker image build -t bgauduch/cod2-server:dev .
 
-# launch cod2 server using local Dockerfile (docker-compose.dev override)
+# launch cod2 server using local image (docker-compose.dev override)
 docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d
