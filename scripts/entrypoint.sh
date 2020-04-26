3f099dd21a9a723870e8854d1503f26b7203a006
bgauduch <bgauduch@users.noreply.github.com>
2020-04-26 15:31:38 +0200

    :sparkles: running server using non-root user

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -euo pipefail
 
-# launch server 
-LD_PRELOAD="/lib/libcod2_1_3.so" /server/cod2_lnxded +set fs_basepath "/server" +set fs_homepath "/home" +exec config.cfg
+# launch server using libcod library
+LD_PRELOAD='/lib/libcod2_1_3.so' ./cod2_lnxded +exec config.cfg
+# +set fs_basepath "/server" +set fs_homepath "/server/home"
 
 # tail server logs in foreground
 # tail -f -n 50 /home/main/games_mp.log
