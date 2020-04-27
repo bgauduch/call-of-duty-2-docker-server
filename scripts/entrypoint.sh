7066032c15539f6999d52724f9ab2e63471bcb42
Baptiste Gauduchon <bgauduch@users.noreply.github.com>
2020-04-27 12:24:50 +0000

    :sparkles: Allow console commands interactions (#29)
    * :whale: add console comand interaction, pass server args with command, removed useless entrypoint
    
    * :fire: remove useles dev exec script
    
    * :sparkles: add dev attach script
    
    * :pencil: readme cleanup and update for server command
    
    * :pencil: server doc update regarding console command usage

--- scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-
-# launch server using libcod library
-LD_PRELOAD='/lib/libcod2_1_3.so' ./cod2_lnxded +exec config.cfg
-# +set fs_basepath "/server" +set fs_homepath "/server/home"
-
-# tail server logs in foreground
-# tail -f -n 50 /home/main/games_mp.log
